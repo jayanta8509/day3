fd9f800bc32b878b6d69d3c2f1d64dbe6e06f5f6
Jayanta Roy <93045961+jayanta8509@users.noreply.github.com>
2023-03-08 14:32:53 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@day3/main.cpp
@@ -0,0 +1,14 @@
+// Write a program to find ASCII values of a character
+#include <iostream>
+using namespace std;
+int ASCII(char a) {
+  cout << (int)a;
+  return 0;
+}
+
+int main() {
+  // cout << "Hello World!\n";
+  char c;
+  cin >> c;
+  ASCII(c);
+}
